693033d7bbe757db36156bb17fc54178c3f9174f
fetaleksej <fetaleksej@gmail.com>
2015-07-30 12:36:43 +0600

    Add some class(c2)

---
+++ test/test.cpp
@@ -0,0 +1,6 @@
+#ifndef ANGSTROM_TEST
+#define ANGSTROM_TEST
+//------------------------------------------------------------
+class SomeEmpty{};
+//------------------------------------------------------------
+#endif // ANGSTROM_TEST
